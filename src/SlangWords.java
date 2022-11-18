15a4b01fe200c724d74c740a29a0da043c4314b2
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-11-18 23:39:14 +0700

    Cập nhật hàm đọc file slang word

--- src/SlangWords.java
+++ src/SlangWords.java
@@ -1,15 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.*;
 
 public class SlangWords {
     private TreeMap<String, List<String>> sw = new TreeMap<String, List<String>>();
     public SlangWords() {
-        List<String> test = new ArrayList<String>();
-        test.add("Test");
-        sw.put("J", test);
+        try {
+            BufferedReader fin = new BufferedReader(new FileReader("slang.txt"));
+            String line = "";
+            while (true) {
+                List<String> listDefi = new ArrayList<String>();
+                line = fin.readLine();
+                if (line == null)
+                    break;
+                String slAndDefi[] = line.split("`");
+                String[] arrDefi = slAndDefi[1].split("\\|");
+                for(String defi : arrDefi) {
+                    listDefi.add(defi.trim());
+                }
+                sw.put(slAndDefi[0], listDefi);
+            }
+            fin.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public void display() {
-        System.out.println(this.sw);
+        Set<String> keySet = sw.keySet();
+        for (String key : keySet) {
+            System.out.println(key + "-" + sw.get(key));
+        }
     }
 }
